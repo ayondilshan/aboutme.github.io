251b611d485155170a9cb58db6dc5719824c27ae
G.H Ayon Dilshan <137699604+ayondilshan@users.noreply.github.com>
2023-06-26 19:09:46 +0530

    Add files via upload

---
+++ mail/contact.php
@@ -0,0 +1,34 @@
+<?php 
+    $userEmail = ""; //first we leave email field blank
+    if(isset($_POST['subscribe'])){ //if subscribe btn clicked
+      $userEmail = $_POST['email']; //getting user entered email
+      if(filter_var($userEmail, FILTER_VALIDATE_EMAIL)){ //validating user email
+        $subject = "Thanks for Subscribing us - CodingNepal";
+        $message = "Thanks for subscribing to our blog. You'll always receive updates from us. And we won't share and sell your information.";
+        $sender = "From: https://formspree.io/f/xdovqrjl";
+        //php function to send mail
+        if(mail($userEmail, $subject, $message, $sender)){
+          ?>
+           <!-- show sucess message once email send successfully -->
+          <div class="alert success-alert">
+            <?php echo "Thanks for Subscribing us." ?>
+          </div>
+          <?php
+          $userEmail = "";
+        }else{
+          ?>
+          <!-- show error message if somehow mail can't be sent -->
+          <div class="alert error-alert">
+          <?php echo "Failed while sending your mail!" ?>
+          </div>
+          <?php
+        }
+      }else{
+        ?>
+        <!-- show error message if user entered email is not valid -->
+        <div class="alert error-alert">
+          <?php echo "$userEmail is not a valid email address!" ?>
+        </div>
+        <?php    }
+    }
+    ?>
